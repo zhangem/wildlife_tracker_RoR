2b56f597d61cff0e4e6cf4753d1796079ba5af0c
Emily Zhang & OKoyea Huff-Boone <student@epicodus.com>
2014-04-01 11:53:09 -0700

    finished RESTful for Species

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -16,4 +16,25 @@ class SpeciesController < ApplicationController
     render('species/new.html.erb')
   end
 
+  def edit
+    @species = Species.find(params[:id])
+    render('species/edit.html.erb')
+  end
+
+  def update
+    {:updated_species => {:name => params[:name], :kind => params[:kind]}}
+    @species = Species.find(params[:id])
+    if @species.update(params[:updated_species])
+      render('species/success.html.erb')
+    else
+      render('species/edit.html.erb')
+    end
+  end
+
+  def destroy
+    @species_to_delete = Species.find(params[:id])
+    @species_to_delete.destroy
+    @species = Species.all
+    render('species/index.html.erb')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 WildlifeTracker::Application.routes.draw do
+  match('/', {:via => :get, :to => 'species#index'})
   match('species', {:via => :get, :to => 'species#index'})
   match('species', {:via => :post, :to => 'species#create'})
   match('species/new', {:via => :get, :to => 'species#new'})
+  match('species/:id', {:via => :get, :to => 'species#edit'})
+  match('species/:id', {:via => [:patch, :put], :to => 'species#update'})
+  match('species/:id', {:via => :delete, :to => 'species#destroy'})
 end
